2eb1afa7d490ef2be3e61f63687aa9771c2b0ff9
Danila Kuznetsov <>
2021-11-07 19:28:49 +0300

    new

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+const { MongoClient } = require('mongodb');
+
+let englishWords = null
+let themes = null
+
+const mongoConnect = async () => {
+  const uri = "mongodb+srv://Julia2222:Julia2222@cluster0.reril.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+  await client.connect(err => {
+    console.log('trying')
+    englishWords =  client.db("myProject").collection("englishWords");
+    themes =  client.db("myProject").collection("themes");
+    console.log('goood connection')
+  });
+}
+
+
+const createEndpoints = () => {
+  /* GET home page. */
+  router.get("/", async (req, res) => {
+    const response = await englishWords.find().toArray()
+        .catch(err => console.error(`Failed to find documents: ${err}`))
+    res.json(response);
+  })
+  router.get("/body", async (req, res) => {
+    const response = await englishWords.find({theme: 'body'}).toArray()
+        .catch(err => console.error(`Failed to find documents: ${err}`))
+    res.json(response);
+  })
+  router.get("/appearance-and-character", async (req, res) => {
+    const response = await englishWords.find({theme: 'Appearance and character'}).toArray()
+        .catch(err => console.error(`Failed to find documents: ${err}`))
+    res.json(response);
+  })
+  router.get("/themes", async (req, res) => {
+    const response = await themes.find().toArray()
+        .catch(err => console.error(`Failed to find documents: ${err}`))
+    res.json(response);
+  })
+}
+
+const launch = async () => {
+  await mongoConnect()
+  await createEndpoints()
+}
+
+launch()
+
+
+module.exports = router;
